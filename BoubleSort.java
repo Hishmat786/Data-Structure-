18159cb0973fb45cc0a31e2287ce0967053f064f
Hishmat Drawar <82969982+Hishmat786@users.noreply.github.com>
2022-06-20 10:10:27 +0500

    Bouble Sorting
    Bouble Sorting

---
+++ BoubleSort.java
@@ -0,0 +1,25 @@
+class BoubleSort {
+    void boubleSort(int[] arr) { 
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BoubleSort obj = new BoubleSort();
+        int[] arr = { 5, 8, 6, 2, 4, 3 };
+        obj.boubleSort(arr);
+        
+    }
+}
